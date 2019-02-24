6caa195700bf4c35e0532f17602ab39e5134f85d
Rémy Milia <milia.remy@gmail.com>
2019-02-24 22:54:17 +0100

    propose generic solution satisfying opc

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,25 +1,30 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanNumerals {
-    public static String compute(int number) {
-        String numerals = "";
-        while (number > 0) {
-            if (number % 10 == 0) {
-                numerals += "X";
-                number -= 10;
-            } else if (number % 5 == 0) {
-                numerals += "V";
-                number -= 5;
-            } else if (number % 10 == 9) {
-                numerals += "XI";
-                number -= 9;
-            } else if (number % 5 == 4) {
-                numerals += "VI";
-                number -= 4;
-            } else {
-                numerals += "I";
-                number -= 1;
+
+    static Map<Integer, String> arabicToRoman = new LinkedHashMap<Integer, String>() {{
+        put(50, "L");
+        put(40, "XL");
+        put(10, "X");
+        put(9, "IX");
+        put(5, "V");
+        put(4, "IV");
+        put(1, "I");
+    }};
+
+    public static String generate(int number) {
+        StringBuilder numerals = new StringBuilder();
+        convert(number, numerals);
+        return numerals.toString();
+    }
+
+    private static void convert(int number, StringBuilder numerals) {
+        for (Map.Entry<Integer, String> m : arabicToRoman.entrySet()) {
+            while (number >= m.getKey()) {
+                numerals.append(m.getValue());
+                number -= m.getKey();
             }
         }
-
-        return new StringBuilder(numerals).reverse().toString();
     }
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -5,44 +5,56 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RomanNumeralsTest {
 
     @Test
-    public void should_generate_as_much_I_as_count_units_from_1_to_3() {
-        assertThat(RomanNumerals.compute(1)).isEqualTo("I");
-        assertThat(RomanNumerals.compute(2)).isEqualTo("II");
-        assertThat(RomanNumerals.compute(3)).isEqualTo("III");
+    public void should_generate_Is_for_1_2_3() {
+        assertThat(RomanNumerals.generate(1)).isEqualTo("I");
+        assertThat(RomanNumerals.generate(2)).isEqualTo("II");
+        assertThat(RomanNumerals.generate(3)).isEqualTo("III");
     }
 
     @Test
     public void should_generate_IV_for_4() {
-        assertThat(RomanNumerals.compute(4)).isEqualTo("IV");
+        assertThat(RomanNumerals.generate(4)).isEqualTo("IV");
     }
 
     @Test
     public void should_generate_V_for_5() {
-        assertThat(RomanNumerals.compute(5)).isEqualTo("V");
+        assertThat(RomanNumerals.generate(5)).isEqualTo("V");
     }
 
     @Test
-    public void should_generate_V_and_as_much_I_as_count_units_from_6_to_8() {
-        assertThat(RomanNumerals.compute(6)).isEqualTo("VI");
-        assertThat(RomanNumerals.compute(7)).isEqualTo("VII");
-        assertThat(RomanNumerals.compute(8)).isEqualTo("VIII");
+    public void should_generate_VI_for_6() {
+        assertThat(RomanNumerals.generate(6)).isEqualTo("VI");
     }
 
     @Test
-    public void should_generate_IX_for_9() {
-        assertThat(RomanNumerals.compute(9)).isEqualTo("IX");
+    public void should_generate_IV_for_9() {
+        assertThat(RomanNumerals.generate(9)).isEqualTo("IX");
     }
 
     @Test
     public void should_generate_X_for_10() {
-        assertThat(RomanNumerals.compute(10)).isEqualTo("X");
+        assertThat(RomanNumerals.generate(10)).isEqualTo("X");
     }
 
     @Test
-    public void should_generate_X_and_as_much_I_as_count_units_from_11_to_13() {
-        assertThat(RomanNumerals.compute(11)).isEqualTo("XI");
-        assertThat(RomanNumerals.compute(12)).isEqualTo("XII");
-        assertThat(RomanNumerals.compute(13)).isEqualTo("XIII");
+    public void should_generate_XX_for_20() {
+        assertThat(RomanNumerals.generate(20)).isEqualTo("XX");
     }
+
+    @Test
+    public void should_generate_XXIX_for_29() {
+        assertThat(RomanNumerals.generate(29)).isEqualTo("XXIX");
+    }
+
+    @Test
+    public void should_generate_L_for_50() {
+        assertThat(RomanNumerals.generate(50)).isEqualTo("L");
+    }
+
+    @Test
+    public void should_generate_XLIV_for_44() {
+        assertThat(RomanNumerals.generate(44)).isEqualTo("XLIV");
+    }
+
 
 }
